b8f0dc73eefc5dfab35c37651cb07862bafe9a9b
Katherine Yang <whykatherine@gmail.com>
2021-09-30 23:30:29 -0700

    feat: fix filtering

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,130 +1,141 @@
-// "use strict"
+// papa parse
+const public_spreadsheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSeEGW1VxkMnHgIIl16U_78ZrP8rvWvk66_InItIv1Riq0K2d7D6fA1jqfcBl7PZHPAQXyqofWmcJTV/pub?output=csv"
+console.log('<a href="' + public_spreadsheet_url + '">' + public_spreadsheet_url + '</a>');
 
-// add data attributes from filled-in html
-// function addDataAttrs(resources) {
-//   for (let res of resources) {
-//     let fields = res.querySelectorAll("div");
-//     for (let field of fields) {
-//       let fieldname = field.classList[0].split("-")[2];
-//       let fieldlist = [];
-//       for (let d of field.querySelectorAll("dd")) {
-//         fieldlist.push(d.textContent);
-//       }
-//       res.dataset[fieldname] = fieldlist.join("|");
-//     }
-//   }
-// }
+// elements
+const resourcesContainer = document.getElementById('resources');
+const resTemplate = document.querySelector(".res");
+resTemplate.remove();
+const numResources = document.querySelector("#numResources");
+const generatorForm = document.querySelector("#generator"),
+  learnSelect = document.querySelector("#learn"),
+  doSelect = document.querySelector("#do"),
+  useSelect = document.querySelector("#use"),
+  makeSelect = document.querySelector("#make"),
+  randomiseButton = document.querySelector("#randomiseButton");
+const selects = [learnSelect, doSelect, useSelect, makeSelect];
 
-// update generator upon interactions
-let updateGenerator = (event) => {
-  console.log('reached updateGenerator()')
-  event.stopPropagation();
-  let actions = event.target.form.querySelectorAll("select");
-
-  // deal with buttons
-  if (event.target.type === "submit") { // submit
-    event.preventDefault();
-  } else if (event.target.type === "reset") { // reset
-    event.preventDefault();
-    event.target.form.reset();
-    for (let action of actions) {
-      for (let opt of action.options) {
-        opt.removeAttribute("selected");
-      }
-      action.options[0].setAttributeNode(document.createAttribute("selected"));
-    }
-  } else if (event.target.value === "randomise") {  // randomise
-    for (let action of actions) {
-      for (let opt of action.options) {
-        opt.removeAttribute("selected");
-      }
-      if (action.options.length > 1) {
-        let numOpt = Math.floor(Math.random() * action.options.length);
-        action.options[numOpt].setAttributeNode(document.createAttribute("selected"));
-      } else {
-        action.options[0].setAttributeNode(document.createAttribute("selected"));
-      }
-    }
-  }
-
-  let resources = generateResources();
-  // updateFilters(resources);
+//empty tag sets, to be populated as data is brought in
+let tags = {
+  learn: new Set(),
+  make: new Set(),
+  use: new Set(),
+  do: new Set(),
+  languages: new Set(),
+  collections: new Set(),
+  opensource: new Set()
 }
 
-// function updateFilters(resources) {
-//   let filters = document.querySelectorAll(".filter select");
-//   for (let filter of filters) {
-//     let filterid = filter.id;
-//     let
-//     let options = filter.querySelectorAll("option:not([value=''])");
-//     options.forEach((val, i, arr) => {
-//       console.log()
-//     });
-//   }
-// }
+// initialise papa parse
+window.addEventListener('DOMContentLoaded', () => {
+  Papa.parse(public_spreadsheet_url, {
+    download: true,
+    header: true,
+    dynamicTyping: true,
+    complete: loadData
+  })
+});
 
-// generate resources based on interests and filters
-function generateResources() {
-  let queries = [];
+function loadData(results) {
+      
+  let data = results.data
+  console.log("Successfully processed " + data.length + " rows!")
+  numResources.innerText = data.length;
 
-  // get actions
-  let actions = document.querySelectorAll(".action select");
-  for (let action of actions) {
-    if (action.value) { // ignore if it's set to "anything"
-      queries.push(action.value);
-    }
+  for (let r of data) {
+    //creates dom elements for each resource
+    createResource(r);
   }
 
-  // get filters
-  let filters = document.querySelectorAll("#filter");
-  for (let filter of filters) {
-    if (filter.value) { // ignore if set to "all"
-      queries.push(filter.value);
-    }
+  for (let tag of ["learn", "do", "use", "make", "languages", "collections"]) {
+    populateDropdowns(tags[tag], tag);
   }
 
-  console.log(queries);
-  console.log("here?")
+  generatorForm.addEventListener("submit", e => {
+    e.preventDefault();
+    filterResources([learnSelect.value, doSelect.value, useSelect.value, makeSelect.value])
+  });
 
-  let resources = document.getElementsByClassName("res");
-  if (queries.length == 0) {
+  generatorForm.addEventListener("reset", e => {
+    filterResources();
+  });
+
+  randomiseButton.addEventListener("click", () => {
+    for (let select of selects) {
+      let options = select.children;
+      select.value = options[Math.floor(Math.random() * options.length)].value;
+    }
+  });
+}
+
+
+//FILTER ONLY SELECT RESOURCES BY KEYWORD TAGS
+function filterResources(classes = []) {
+  const resources = document.querySelectorAll(".res");
+
+  // if nothing is selected, show everything
+  let showAll = classes.every(c => c === "");
+  if (showAll) {
     for (let res of resources) {
-      res.style.display = "block";
+      res.classList.toggle("res--visible", true);
     }
   } else {
     for (let res of resources) {
-      let tags = [...res.classList]; // turn into an array
-      console.log(tags)
-      //RIGHT NOW IT's not working here??
-      console.log("this is where tag is empty")
-      tags.splice(0, 1); // remove first "res" class name
-      let matches = tags.filter(tag => {
-        //RIGHT NOW IT's not working here??
-        console.log("tag filtering happening??")
-        // console.log(queries.includes(tag.replace("-", ":").replace("_", " ")))
-        return queries.includes(tag.replace("-", ":").replace("_", " "))
-      });
-      if (matches.length > 0) {
-        res.style.display = "block";
-      } else {
-        res.style.display = "none";
+      // check if resource contains at least one of the given tags
+      let match = false;
+      for (let c of classes) {
+        if (res.classList.contains(c)) {
+          match = true;
+        }
+      }
+      res.classList.toggle("res--visible", match);
+    }
+  }
+
+  // update numResources text
+  numResources.innerText = document.querySelectorAll(".res--visible").length;
+}
+
+
+function populateDropdowns(tags, field){
+  let select = document.getElementById(field)
+  let sorted = Array.from(tags).sort(); // sort alphabetically
+  for (let item of sorted) {
+    let option = document.createElement("option")
+    option.value = item 
+    option.innerText = item
+    select.appendChild(option)
+  }
+}
+
+function createResource(r){
+  // create element
+  let res = resTemplate.cloneNode(true);
+  if (r.link) res.querySelector(".res-title").href = r.link;
+  res.querySelector(".res-title h3").innerText = r.title;
+  res.querySelector(".res-creator").innerText = r.creator;
+  res.querySelector(".res-blurb").innerText = r.blurb;
+  for (let field of ["learn", "use", "do", "make", "collections", "languages"]) {
+    if (r[field]) {
+      res.querySelector(".res-" + field).appendChild(document.createTextNode(r[field]));
+    }
+  }
+
+  // add tags to sets and classlist
+  for (let tag of ["learn", "make", "use", "do", "languages", "collections"]) {
+    if (r[tag]) {
+      let tagList = r[tag].split(",")
+      for (let t of tagList) { 
+        tags[tag].add(t.trim())
+        let escaped = t.trim().replace(/\s+/g, "");
+        res.classList.add(escaped);
       }
     }
   }
 
-  return resources;
+  // make visible initially
+  res.classList.add("res--visible");
+
+  // append
+  resourcesContainer.appendChild(res);
 }
-
-
-
-//issue is with how to set up the form I think
-let resources = document.getElementsByClassName("res");
-var generator = document.getElementById('generator');
-
- for (let button of generator.querySelectorAll("button")) {
-   button.addEventListener("click", updateGenerator);
- }
-
-let filterForm = document.getElementById("filters");
-// filterForm.reset();
-document.addEventListener("input", generateResources);
